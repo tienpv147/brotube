1349a1520bd9651fc5a14b242ac868988b61f6c9
Tien Pham Van <tienpv14701@gmail.com>
2022-10-05 21:48:43 +0700

    Initial commit

---
+++ src/test/java/com/brotube/SpringwebfluxVideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brotube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxVideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
